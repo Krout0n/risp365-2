9acc407ee8f7aaac0eb0d76fbcab1f3c8d26695d
Krout0n <me+git@tokinia.me>
2022-10-22 16:03:17 +0900

    AST と eval を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum AST {
+    Num(usize),
+}
+
+pub fn eval(ast: AST) -> usize {
+    match ast {
+        AST::Num(v) => v,
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_eval() {
+        let ast = AST::Num(1);
+        assert_eq!(eval(ast), 1);
     }
 }
