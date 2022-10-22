ef0be179028ef2d04565a1a564545ddbb2d8a578
Krout0n <me+git@tokinia.me>
2022-10-22 17:04:34 +0900

    If式を追加した

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@
 pub enum AST {
     Num(usize),
     Add(Box<AST>, Box<AST>),
+    If {
+        // if における条件式
+        cond: Box<AST>,
+        // true だったときに eval する ast
+        then: Box<AST>,
+        // false だったときに eval する ast
+        els: Box<AST>,
+    },
 }
 
 pub fn eval(ast: AST) -> usize {
@@ -12,6 +20,10 @@ pub fn eval(ast: AST) -> usize {
             let right_obj = eval(*right);
             left_obj + right_obj
         }
+        AST::If { cond, then, els } => match eval(*cond) {
+            0 => eval(*els),
+            _ => eval(*then),
+        },
     }
 }
 
@@ -23,6 +35,13 @@ macro_rules! ast {
         // このマクロの中でASTやpubにしてるやつを使いたいときは
         // `$crate::`っておまじないをつけてください:pray:
         $crate::AST::Add(Box::new(ast!($left)), Box::new(ast!($right)))
+    };
+    ((If $cond:tt $then:tt $els:tt)) => {
+        $crate::AST::If {
+            cond: Box::new(ast!($cond)),
+            then: Box::new(ast!($then)),
+            els: Box::new(ast!($els)),
+        }
     };
     ($num:expr) => {
         $crate::AST::Num($num)
@@ -59,6 +78,21 @@ mod tests {
     }
 
     #[test]
+    fn test_eval_if() {
+        // 条件式の値が 0 なので els の 200
+        let ast = ast!((If 0 100 200));
+        assert_eq!(eval(ast), 200);
+
+        // 条件式の値が 0 じゃないので then の 100
+        let ast = ast!((If 1 100 200));
+        assert_eq!(eval(ast), 100);
+
+        // 同様に条件式の値が 0 じゃないので then の 100
+        let ast = ast!((If (+ 0 3) 100 200));
+        assert_eq!(eval(ast), 100);
+    }
+
+    #[test]
     fn test_ast_macro() {
         assert_eq!(
             ast!((+ 1 2)),
@@ -78,5 +112,14 @@ mod tests {
                 Box::new(AST::Num(5)),
             )
         );
+
+        assert_eq!(
+            ast!((If 0 1 2)),
+            AST::If {
+                cond: Box::new(AST::Num(0)),
+                then: Box::new(AST::Num(1)),
+                els: Box::new(AST::Num(2))
+            }
+        )
     }
 }
